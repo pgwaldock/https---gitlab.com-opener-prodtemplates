818bf3f8906fa59678c8005ff89bd64b89c26810
peter waldock <pete@flender.ie>
2017-02-01 22:22:34 +0000

    files removed

--- js/confirmation-handlers.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-    <!-- apply disocunt -->	
-    $('.action_applyDiscount').click(function(e) {
-        $('.original-price').removeClass('displayNone');
-        $('.original-price').addClass('mimic-click');
-    });
-
-});
